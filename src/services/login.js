208be25ef4711bd8a34285ca8e91a4d8d59f79f7
Topias <43338418+topiassaari@users.noreply.github.com>
2022-02-09 21:56:48 +0200

    change axios basUrl

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:8000/api/login";
+const baseUrl = "/api/login";
 
 const login = async (cred) => {
   const response = await axios.post(baseUrl, cred);
--- src/services/postalService.js
+++ src/services/postalService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:8000/api/images";
+const baseUrl = "/api/images";
 let token = null;
 
 const setToken = (newToken) => {
